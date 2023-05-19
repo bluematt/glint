d1efacfdc928888e2b9bc9b388a0ea4061bd1393
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-19 11:08:03 +0100

    Updated Game
    Also added a new (trivial and contrived) example

---
+++ examples/simple/basic.cr
@@ -0,0 +1,4 @@
+require "../../src/glint.cr"
+
+# Opens a game window, and does nothing else.
+Game.new.run!
--- src/glint/game.cr
+++ src/glint/game.cr
@@ -1,37 +1,40 @@
 module Glint
   # The main game class.
+  #
+  # Extend the `Game` class with your own `draw!` and `update!(delta : Float)` methods
+  # to create simple games, or use the `Scene` class to create more complex games.
   class Game
-    DEFAULT_WIDTH  = 1920
-    DEFAULT_HEIGHT = 1080
-    DEFAULT_TITLE  = "<untitled>"
-    DEFAULT_FPS    = 60
+    DEFAULT_WIDTH     = 1920
+    DEFAULT_HEIGHT    = 1080
+    DEFAULT_TITLE     = "<untitled>"
+    DEFAULT_FRAMERATE = 60
 
-    # The game window.
-    @window : Glint::Window
+    # The game `Window`.
+    @window : Window
 
-    # The current scene.
-    @scene : Glint::Entity::Scene
+    # The current `Scene`.
+    @scene : Scene
 
-    # The target_framerate
-    @target_framerate = DEFAULT_FPS
+    # The target (maximum ideal) framerate.
+    @target_framerate : Int32 = DEFAULT_FRAMERATE
 
     # Whether to display FPS.
-    @display_fps = false
+    @display_fps : Bool = false
 
-    # Create a new game with custom size, title and framerate.
-    def initialize(width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, title = DEFAULT_TITLE, @target_framerate = DEFAULT_FPS)
+    # Creates a new game with optional size, title and framerate.
+    def initialize(width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, title = DEFAULT_TITLE, @target_framerate = DEFAULT_FRAMERATE)
       @window = Window.new(width, height, title)
-      @scene = Glint::Entity::Scene.new
+      @scene = Scene.new
       @scene.background_color = Color::WHITE
       @scene << Label.new("Glint #{Glint::VERSION}", vec(width/2, height/2), 20, Color::WHITE)
     end
 
-    # Run the game's mainloop: handle inputs, update game state, output.
+    # Runs the game's mainloop: handle inputs, update game state, output.
     def run!
       framerate = @target_framerate
 
       Raylib.wait_time(0.01)
-      delta = Raylib.get_frame_time
+      delta = Game.frame_time
 
       until should_quit?
         _update!(delta) if delta > 0.0
@@ -40,22 +43,24 @@ module Glint
         _draw!
         Raylib.end_drawing
 
-        delta = Raylib.get_frame_time
+        delta = Game.frame_time
       end
 
       quit!
     end
 
-    private def _update!(delta)
+    # Updates the current game and the `Scene`.
+    private def _update!(delta : Float)
       if scene = @scene
         scene._update!(delta)
       end
       update!(delta)
     end
 
-    # Update game state.
-    def update!(delta); end
+    # Performs custom updates.
+    def update!(delta : Float); end
 
+    # Draws the current game.
     private def _draw!
       if scene = @scene
         clear_background(scene.background_color)
@@ -65,8 +70,13 @@ module Glint
       display_fps if @display_fps
     end
 
-    # Clear the background to the specified color.
-    def clear_background(color)
+    # Performs custom drawing.
+    #
+    # Overload this method to draw directly, bypassing the automatic drawing of `Scene`s.
+    def draw!; end
+
+    # Clears the background to the specified color.
+    def clear_background(color : Color)
       Raylib.clear_background(color.to_raylib_color) if color
     end
 
@@ -77,37 +87,39 @@ module Glint
       puts framerate
     end
 
-    # Draw game state.
-    def draw!; end
-
-    # Return whether the game should quit.
+    # Returns whether the game should quit.
     def should_quit?
       @window.close?
     end
 
-    # Quit the game.
+    # Quits the game.
     def quit!
       @window.close!
     end
 
-    # Set the framerate.
+    # Sets the framerate.
     def framerate=(fps : UInt16)
       @target_framerate = fps
       Raylib.set_target_fps(@target_framerate)
     end
 
-    # Get the target framerate.
+    # Returns the target framerate.
     #
     # This may differ from the `framerate`.
     def target_framerate
       @target_framerate
     end
 
-    # Get the current (real) framerate.
+    # Returns the current (real) framerate.
     #
     # This may differ from the `target_framerate`.
     def framerate
       Raylib.get_fps
+    end
+
+    # Returns the duration (in seconds) since the last frame.
+    def self.frame_time
+      Raylib.get_frame_time
     end
   end
 end
