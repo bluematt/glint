84c889659069b97949bccbe0fdc2523ac02e695b
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-19 15:16:42 +0100

    Update label.cr
    Added docs

--- examples/simple/label.cr
+++ examples/simple/label.cr
@@ -1,10 +1,14 @@
 require "../../src/glint.cr"
 
 class MyGame < Game
+  # Set the game to be 800x600 and have an awesome title.
+  #
+  # We'll keep the defauly framerate (60fps).
   def initialize
     super(800, 600, "My Awesome Game")
   end
 
+  # Draw a simple label.
   def draw
     Label.draw("Hello, World!")
   end
