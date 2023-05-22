19db055249498031384f39e3506705711d4fb3c0
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-22 15:31:28 +0100

    Update sprite.cr
    Docs

--- src/glint/entity/sprite.cr
+++ src/glint/entity/sprite.cr
@@ -1,29 +1,33 @@
 module Glint
   module Entity
     class Sprite < Entity
+      # The sprite's texture.
       @texture : Raylib::Texture2D
-      @tint : Color = Color::WHITE
+
+      # The sprite's tint.
+      property tint : Color = Color::WHITE
 
       def initialize(@texture : Raylib::Texture2D)
         @extents = vec(@texture.width, @texture.height)
       end
 
+      # Creates a sprite from an external texture.
       def initialize(filename : String)
         @texture = Preload.texture(filename)
         @extents = vec(@texture.width, @texture.height)
       end
 
-      # Preload a texture.
+      # Preloads an external texture.
       def self.preload(filename : String)
         self.new(filename)
       end
 
-      # Draw the texture at it's natural position.
+      # Draws the texture at it's natural position.
       def draw
         draw_at(@position)
       end
 
-      # Draw the texture at a specific position.
+      # Draws the texture at a specific position.
       def draw_at(position)
         Raylib.draw_texture_pro(@texture, @extents.to_rect, position.to_rect(@extents), @pivot.from(@extents).to_raylib, @rotation, @tint.to_raylib)
       end
