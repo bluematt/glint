24e6dbef49413f8b461e653cbf8774724dd158a7
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-22 09:08:00 +0100

    Update scene.cr
    Docs

--- src/glint/entity/scene.cr
+++ src/glint/entity/scene.cr
@@ -7,11 +7,14 @@ module Glint
       # The scene's background color.
       property background_color : Glint::Color?
 
+      # Creates a new scene.
       def initialize; end
 
+      # Creates a new scene with a specific background color.
       def initialize(@background_color); end
     end
   end
 end
 
+# Convenience alias for `Glint::Entity::Scene`.
 alias Scene = Glint::Entity::Scene
