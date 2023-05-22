3e9b8e861a29250804faa0f652a2dee00d5c5b5e
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-22 10:38:57 +0100

    Update visual_game_object.cr
    Prep for drawing entities at non-absolute positions

--- src/glint/entity/visual_game_object.cr
+++ src/glint/entity/visual_game_object.cr
@@ -1,17 +1,56 @@
 module Glint
   module Entity
+    # A visual game object that will appear on screen.
     abstract class VisualGameObject < GameObject
-      # The game object's position.
-      property position : Vector2 = Vector2.new
+      # The entity's default position.
+      DEFAULT_POSITION = vec()
 
-      # The game object's rotation.
-      property rotation = 0.0
+      # The entity's default rotation, in degrees.
+      DEFAULT_ROTATION = 0.0
 
-      # The game object's point of transformation.
-      property pivot : Origin = Origin::TopLeft
+      # The entity's default pivot.
+      DEFAULT_ORIGIN = Origin::TopLeft
 
-      # The game object's extents (maximum dimensions)
-      property extents : Vector2 = vec()
+      # The entity's position.
+      property position : Vector2 = DEFAULT_POSITION
+
+      # The entity's position relative to its parent.
+      property draw_position : DrawPosition = DrawPosition::Absolute
+
+      # The entity's rotation.
+      property rotation = DEFAULT_ROTATION
+
+      # The entity's point of transformation.
+      property pivot : Origin = DEFAULT_ORIGIN
+
+      # Returns game object's extents (maximum dimensions)
+      getter extents : Vector2 = vec()
+
+      # Draws the entity and its children.
+      #
+      # This overrides `Entity#draw`.
+      protected def _draw
+        # Draws the children under the current entity.
+        if @draw_children
+          @children.each { |e| e._draw } unless draw_above_children
+        end
+
+        case @draw_position
+        when .relative?
+          # Draw relative to the parent.
+          draw_at(@position)
+        else
+          # Draw in an absolute position.
+          draw
+        end
+
+        # Draws the children above the current entity.
+        if @draw_children
+          @children.each { |e| e._draw } if draw_above_children
+        end
+      end
+
+      def draw_at(position); end
     end
   end
 end
