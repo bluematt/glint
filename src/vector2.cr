9d4479f811fa41daab636226c5fe1595946d17bf
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-23 14:24:05 +0100

    Update vector2.cr

--- src/vector2.cr
+++ src/vector2.cr
@@ -1,85 +1,3 @@
-# module Glint
-#   # A two dimensional vector.
-#   class Vector2
-#     property x, y = 0.0
-
-#     # Creates a new `Vector2`.
-#     def initialize(@x = 0.0, @y = 0.0); end
-
-#     # Converts to a Rectangle with this size.
-#     def to_rect
-#       R::Rectangle.new x: 0, y: 0, width: @x, height: @y
-#     end
-
-#     # Converts to a Rectangle at this position with a size.
-#     def to_rect(size : Vector2)
-#       R::Rectangle.new x: @x, y: @y, width: size.x, height: size.y
-#     end
-
-#     # Adds another vector.
-#     def +(other : Vector2)
-#       return vec(@x + other.x, @y + other.y)
-#     end
-
-#     # Multiplies the vector by a factor.
-#     def *(factor : Number)
-#       vec(@x * factor, @y * factor)
-#     end
-
-#     # Multiplies the vector by a factor `Vector2`.
-#     #
-#     # Each component is multiplied by its corresponding component in the *other* `Vector2`.
-#     def *(other : Vector2)
-#       vec(@x * other.x, @y * other.y)
-#     end
-
-#     # Divides the vector by a factor.
-#     def /(factor)
-#       vec(@x / factor, @y / factor)
-#     end
-
-#     # Multiplies the vector by a factor `Vector2`.
-#     #
-#     # Each component is divided by its corresponding component in the *other* `Vector2`.
-#     def /(other : Vector2)
-#       vec(@x / other.x, @y / other.y)
-#     end
-
-#     # Returns a Raylib compatible version of the vector.
-#     #
-#     # This method is always inlined.
-#     #
-#     # TODO Check whether inlining actually makes any difference.
-#     @[AlwaysInline]
-#     def to_raylib : R::Vector2
-#       R::Vector2.new x: @x, y: @y
-#     end
-
-#     # Returns a readable version.
-#     def to_s(io)
-#       io << "(" << @x << "," << @y << ")"
-#     end
-#   end
-# end
-
-# # Convenience alias for `Glint::Vector2`.
-# alias Vector2 = Glint::Vector2
-
-# # Returns a new `Vector2`.
-# def vec(x : Number, y : Number) : Vector2
-#   Vector2.new(x, y)
-# end
-
-# # Returns a new `Vector2` from a `Raylib::Vector2`.
-# def vec(raylib_vec : Glint::R::Vector2) : Vector2
-#   vec(raylib_vec.x, raylib_vec.y)
-# end
-
-# # Returns a new `Vector2`.
-# def vec : Vector2
-#   Vector2.new
-# end
-
 struct Raylib::Vector2
   # A lot of this is replicated from: https://github.com/unn4m3d/crystaledge/blob/master/src/crystaledge/vector2.cr
 
@@ -90,6 +8,15 @@ struct Raylib::Vector2
 
   # Creates a new vector.
   def initialize(@x = 0.0, @y = 0.0); end
+
+  # def initialize(@x : Float64 | Float32 | Int32 = 0.0, @y : Float64 | Float32 | Int32 = 0.0); end
+
+  # Creates a new vector from another vector.
+  # TODO Do we need this?  Can't we just consume vectors as-is?
+  def initialize(v : Raylib::Vector2)
+    @x = v.x
+    @y = v.y
+  end
 
   # Zero vector
   def self.zero
@@ -254,6 +181,35 @@ struct Raylib::Vector2
   def rotate(angle : Float64)
     Raylib::Vector2.new(@x * Math.cos(angle) - @y * Math.sin(angle), @x * Math.sin(angle) + @y * Math.cos(angle))
   end
+
+  # Rotates a vector by *angle* (in degrees).
+  def rotate_degrees(angle : Float64)
+    rotate(angle.radians)
+  end
+
+  def to_rect
+    Raylib::Rectangle.new x: 0, y: 0, width: @x, height: @y
+  end
+
+  def to_rect(size : Raylib::Vector2)
+    Raylib::Rectangle.new x: @x, y: @y, width: size.x, height: size.y
+  end
 end
 
+# A two-dimensional vector.
 alias Vector2 = Raylib::Vector2
+
+# Creates a `Raylib::Vector2` from another `Raylib::Vector2`.
+def vec(v : Raylib::Vector2) : Vector2
+  Raylib::Vector2.new(v.x, v.y)
+end
+
+# Creates a `Raylib::Vector2`.
+def vec(x : Number, y : Number) : Vector2
+  Raylib::Vector2.new(x, y)
+end
+
+# Creates a `Raylib::Vector2`.
+def vec : Vector2
+  Raylib::Vector2.new
+end
