bf81e6e8cd3a7e9374dfd817b181db65db4c67ce
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-23 10:39:51 +0100

    Update example.cr
    Added some docs to example

--- examples/spinning_logo/src/example.cr
+++ examples/spinning_logo/src/example.cr
@@ -6,23 +6,30 @@ module Examples
   class SimpleWindowExample < Game
     # A sprite.
     @icon : Sprite
+
     # A label.
     @label : Label
+
     # How much time has elapsed.
     @elapsed = 0.0
 
     # Set up the "game".
+    #
+    # This sets the window size, sets the title and allows the FPS to be displayed.
+    # We then create a new Scene with a plain RayWhite background.
+    #
+    # Then we create two entities: a `Sprite` which displays a spinning logo, and a
+    # label which displays the current `Glint::VERSION`.
     def initialize
       super(800, 600, "Raylib logo")
       @display_fps = true
-      @scene = Scene.new(Color::WHITE)
+      @scene = Scene.new(Color::RAYWHITE)
 
       # The icon will be added to the default scene.  We will not need to
       # tell this to be drawn later.
       @icon = Sprite.preload("./assets/Raylib_logo.png")
       @icon.position = @window.center
       @icon.pivot = Origin::Center
-      @icon.tint = Color::RED
       @scene << @icon
 
       # The label, on the other hand, is not added to the scene, therefore
@@ -32,13 +39,16 @@ module Examples
       @label.pivot = Origin::BottomCenter
     end
 
-    # Rotate the icon by 30° every second (using *delta*), and change the label color.
+    # Rotate the `@icon` by 30° every second (using *delta*), and change the
+    # color of the `@label` and the `@icon`.
     #
-    # We use a block (see https://crystal-lang.org/reference/latest/syntax_and_semantics/blocks_and_procs.html)
-    # here to create a pseudo-`update` method that doesn't require us to subclass
-    # `Sprite` and create an `update` method for it.
+    # We use a [block](https://crystal-lang.org/reference/latest/syntax_and_semantics/blocks_and_procs.html)
+    # to create a pseudo-`update` method on `@icon` that doesn't require us to
+    # subclass `Sprite` and create an `update` method for it.
     def update(delta)
+      # Each frame rotate by 30°.
       per_frame_rotation = 30 * delta
+      half_rotation = per_frame_rotation / 2
 
       # Change the color once every second.
       @elapsed += delta
@@ -49,19 +59,23 @@ module Examples
         @label.color = random_color
       end
 
-      # Long form, do the first 15° of the rotation.
+      # Long form, do the first half of the rotation.
       @icon.update(delta) do |entity|
-        entity.rotation += per_frame_rotation / 2
+        entity.rotation += half_rotation
       end
 
-      # Short form, do the other 15° of the rotation.
-      @icon.update(delta) { |e| e.rotation += per_frame_rotation / 2 }
+      # Short form, do the other half of the rotation.
+      @icon.update(delta) { |e| e.rotation += half_rotation }
     end
 
     # Draw stuff.
     #
     # We don't need to explicitly `draw` the `@icon` as it's part of the default `Scene`,
     # `@scene`.  `Scene`s and their contents are automatically drawn (if they are not hidden).
+    #
+    # **Note**: Because `@label` is *not* a child of `@scene`, it will not be automatically drawn
+    # or updated.  In this case, as it doesn't change, it doesn't need a manual `update`, but it
+    # still needs to be `draw`n.
     def draw
       # @icon.draw
       @label.draw
