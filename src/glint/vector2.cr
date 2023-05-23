a3026684b8b2bc3973070c28dd7a5fd34f48ff06
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-23 10:26:11 +0100

    Update vector2.cr
    Doc cleanup

--- src/glint/vector2.cr
+++ src/glint/vector2.cr
@@ -3,40 +3,42 @@ module Glint
   class Vector2
     property x, y = 0.0
 
+    # Creates a new `Vector2`.
     def initialize(@x = 0.0, @y = 0.0); end
 
-    # Convert to a Rectangle with this size.
+    # Converts to a Rectangle with this size.
     def to_rect
       R::Rectangle.new x: 0, y: 0, width: @x, height: @y
     end
 
-    # Convert to a Rectangle at this position with a size.
+    # Converts to a Rectangle at this position with a size.
     def to_rect(size : Vector2)
       R::Rectangle.new x: @x, y: @y, width: size.x, height: size.y
     end
 
+    # Adds another vector.
     def +(other : Vector2)
       return vec(@x + other.x, @y + other.y)
     end
 
-    # Multiply the vector by a factor.
+    # Multiplies the vector by a factor.
     def *(factor : Number)
       vec(@x * factor, @y * factor)
     end
 
-    # Multiply the vector by a factor `Vector2`.
+    # Multiplies the vector by a factor `Vector2`.
     #
     # Each component is multiplied by its corresponding component in the *other* `Vector2`.
     def *(other : Vector2)
       vec(@x * other.x, @y * other.y)
     end
 
-    # Divide the vector by a factor.
+    # Divides the vector by a factor.
     def /(factor)
       vec(@x / factor, @y / factor)
     end
 
-    # Multiply the vector by a factor `Vector2`.
+    # Multiplies the vector by a factor `Vector2`.
     #
     # Each component is divided by its corresponding component in the *other* `Vector2`.
     def /(other : Vector2)
@@ -63,17 +65,17 @@ end
 # Convenience alias for `Glint::Vector2`.
 alias Vector2 = Glint::Vector2
 
-# Return a new `Vector2`.
+# Returns a new `Vector2`.
 def vec(x : Number, y : Number) : Vector2
   Vector2.new(x, y)
 end
 
-# Return a new `Vector2` from a `Raylib::Vector2`.
+# Returns a new `Vector2` from a `Raylib::Vector2`.
 def vec(raylib_vec : Glint::R::Vector2) : Vector2
   vec(raylib_vec.x, raylib_vec.y)
 end
 
-# Return a new `Vector2`(0, 0).
+# Returns a new `Vector2`.
 def vec : Vector2
   Vector2.new
 end
