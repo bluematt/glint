f3bfc691682cad4422471cac61c773b0a4790add
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-25 13:52:55 +0100

    Renamed shape properties
    Updated line

--- src/glint/entity/shape/line.cr
+++ src/glint/entity/shape/line.cr
@@ -2,23 +2,22 @@ module Glint
   module Shape
     class Line < Shape
       @end_position : Position = Vector2.new
-      @width : Float32 = 1
 
       # Draws the line.
       def draw
-        if foreground_color = @foreground_color
-          Line.draw(@position, @end_position, @width, foreground_color)
+        if outline_color = @outline_color
+          Line.draw(@position, @end_position, @outline, outline_color)
         end
       end
 
       # Draw a line.
-      def self.draw(start_position : Vector2, end_position : Vector2, width : Float32 = 1, color : Color = Color::BLACK)
-        Raylib.draw_line_ex(start_position, end_position, width, color)
+      def self.draw(start_position : Vector2, end_position : Vector2, outline : Float32 = 1, color : Color = Color::BLACK)
+        Raylib.draw_line_ex(start_position, end_position, outline, color)
       end
 
       # :ditto:
-      def self.draw(x1 : Float32, y1 : Float32, x2 : Float32, y2 : Float32, width : Float32 = 1, color : Color = Color::BLACK)
-        self.draw(Vector2.new(x1, y1), Vector2.new(x2, y2), width, color)
+      def self.draw(x1 : Float32, y1 : Float32, x2 : Float32, y2 : Float32, outline : Float32 = 1, color : Color = Color::BLACK)
+        self.draw(Vector2.new(x1, y1), Vector2.new(x2, y2), outline, color)
       end
 
       # Returns the (start) position.
--- src/glint/entity/shape/shape.cr
+++ src/glint/entity/shape/shape.cr
@@ -1,11 +1,70 @@
 module Glint
   module Shape
+    DEFAULT_FILL_COLOR    = Color::RED
+    DEFAULT_OUTLINE_COLOR = Color::GREEN
+
+    DEFAULT_OUTLINE_POSITION = OutlinePosition::Centered
+
+    # Where a `Shape`'s outline is positioned relative to the fill area.
+    enum OutlinePosition
+      # Overlaps the edge of the fill.
+      Centered
+      # Inside the edge of the fill.
+      Inside
+      # Outside the edge of the fill.
+      Outside
+    end
+
     # A `Shape` is a generic shape entity that can be drawn.
     abstract class Shape < Glint::Entity::Entity
-      # The shape's foreground (outline) color.
-      property foreground_color : Color?
-      # The shape's background (fill) color.
-      property background_color : Color?
+      # The shape's fill color.
+      @fill_color : Color?
+      # The shape's outline color.
+      @outline_color : Color?
+      # The thickness of any outline.
+      @outline : Float32 = 0
+      # The position of the outline.
+      @outline_position : OutlinePosition = OutlinePosition::Centered
+
+      # Returns the fill color.
+      def fill_color
+        @fill_color
+      end
+
+      # Sets the fill color.
+      def fill_color=(color : Color)
+        @fill_color = color
+      end
+
+      # Returns the outline color.
+      def outline_color
+        @outline_color
+      end
+
+      # Sets the outline color.
+      def outline_color=(color : Color)
+        @outline_color = color
+      end
+
+      # Returns the outline width.
+      def outline
+        @outline
+      end
+
+      # Sets the outline width.
+      def outline=(width : Number)
+        @outline = width
+      end
+
+      # Returns the outline position.
+      def outline_position
+        @outline_position
+      end
+
+      # Sets the outline position.
+      def outline_position=(position : OutlinePosition)
+        @outline_position = position
+      end
     end
   end
 end
@@ -15,3 +74,5 @@ require "./line.cr"
 require "./polygon.cr"
 require "./rectangle.cr"
 require "./square.cr"
+
+alias OutlinePosition = Glint::Shape::OutlinePosition
