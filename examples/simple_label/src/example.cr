72f25712786ff807a893ba9855ea018a88393d81
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-25 09:20:44 +0100

    Update example.cr

--- examples/simple_label/src/example.cr
+++ examples/simple_label/src/example.cr
@@ -1,18 +1,18 @@
 require "glint"
 
 # Opens and window and displays a simple label saying "Hello, World!".
-class MyGame < Game
+class SimpleLabelExample < Game
   # Set the game to be 800x600 and have an awesome title.
   #
   # We'll keep the defauly framerate (60fps).
   def initialize
-    super(800, 600, "My Awesome Game")
+    super(800, 600, "Simple Label Example")
   end
 
   # Draw a simple label.
   def draw
-    Label.draw("Hello, World!", color: Color::WHITE)
+    Label.draw("Hello, World!", color: Color::RAYWHITE)
   end
 end
 
-MyGame.new.run
+SimpleLabelExample.new.run
