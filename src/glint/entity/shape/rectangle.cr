94163d4a10f1a8551110d668b973ab53091ce075
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-26 14:06:33 +0100

    Update rectangle.cr

--- src/glint/entity/shape/rectangle.cr
+++ src/glint/entity/shape/rectangle.cr
@@ -1,18 +1,30 @@
 module Glint
   module Shape
     class Rectangle < Shape
+      # The size of the `Rectangle`.
+      property size : Dimension = Dimension.new
+
+      # Draw the `Rectangle`.
+      def draw()
+        Rectangle.draw(@position, @size, @fill_color, @outline, @outline_color, @outline_position)
+      end
+
+      # Draw a `Rectangle`.
       def self.draw(position : Position, size : Dimension, fill_color : Color? = DEFAULT_FILL_COLOR, outline : Float32 = 0, outline_color : Color? = DEFAULT_OUTLINE_COLOR, outline_position = OutlinePosition::Centered)
         Rectangle.draw(position.x, position.y, size.x, size.y, fill_color, outline, outline_color, outline_position)
       end
 
+      # :ditto:
       def self.draw(x : Number, y : Number, size : Dimension, fill_color : Color? = DEFAULT_FILL_COLOR, outline : Float32 = 0, outline_color : Color? = DEFAULT_OUTLINE_COLOR, outline_position = OutlinePosition::Centered)
         Rectangle.draw(x, y, size.x, size.y, fill_color, outline, outline_color, outline_position)
       end
 
+      # :ditto:
       def self.draw(position : Position, width : Number, height : Number, fill_color : Color? = DEFAULT_FILL_COLOR, outline : Float32 = 0, outline_color : Color? = DEFAULT_OUTLINE_COLOR, outline_position = OutlinePosition::Centered)
         Rectangle.draw(position.x, position.y, width, height, fill_color, outline, outline_color, outline_position)
       end
 
+      # :ditto:
       def self.draw(x : Number, y : Number, width : Number, height : Number, fill_color : Color? = DEFAULT_FILL_COLOR, outline : Float32 = 0, outline_color : Color? = DEFAULT_OUTLINE_COLOR, outline_position = OutlinePosition::Centered)
         raise ArgumentError.new("Invalid width #{width}") if width <= 0
         raise ArgumentError.new("Invalid height #{height}") if height <= 0
@@ -27,10 +39,12 @@ module Glint
         Rectangle.outline(Position.new(x, y), Dimension.new(width, height), outline, outline_color, outline_position) if outline_color
       end
 
+      # Draw a filled rectangle.
       def self.fill(position : Position, size : Dimension, fill_color : Color? = DEFAULT_FILL_COLOR)
         Raylib.draw_rectangle(position.x, position.y, size.x, size.y, fill_color)
       end
 
+      # Draw an outlined rectangle.
       def self.outline(position : Position, size : Dimension, outline : Float32 = 0, outline_color : Color? = DEFAULT_OUTLINE_COLOR, outline_position = OutlinePosition::Centered)
         return if outline_color.nil?
         return if outline <= 0
