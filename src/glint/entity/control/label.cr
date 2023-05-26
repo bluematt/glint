da30b054243f773715bde14fa7b6caf17562632d
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-26 12:13:29 +0100

    Update label.cr

--- src/glint/entity/control/label.cr
+++ src/glint/entity/control/label.cr
@@ -4,37 +4,53 @@ module Glint
     class Label < Control
       # The default text.
       DEFAULT_TEXT = ""
+
       # The default font size.
       DEFAULT_FONT_SIZE = 20
-      DEFAULT_COLOR     = Color::BLACK
-      # DEFAULT_FONT      = Font.default
+
+      # The default text color.
+      DEFAULT_COLOR = Color::BLACK
+
+      # The default text spacing.
       DEFAULT_SPACING = 1
 
+      # The label's text.
       property text : String
+
+      # The label's font size.
       property font_size : Int32 = DEFAULT_FONT_SIZE
+
+      # The label's color.
       property color : Color = DEFAULT_COLOR
+
+      # The label's font.
       property font : Raylib::Font
+
+      # The label's text spacing.
       property spacing = DEFAULT_SPACING
 
+      # Creates a `Label`.
       def initialize(@text = DEFAULT_TEXT, @position = DEFAULT_POSITION, @font_size = DEFAULT_FONT_SIZE, @color = DEFAULT_COLOR)
         @font = Font.default
         @spacing = DEFAULT_SPACING
       end
 
-      # Draw the texture at it's natural position.
+      # Draw the `Label`.
       def draw
         draw_at(@position)
       end
 
-      # Draw the texture at a specific position.
+      # Draw the `Label` at a specific position.
       def draw_at(position : Position)
         Raylib.draw_text_pro(@font, @text, position, @pivot.from(get_extents), @rotation, @font_size, @spacing, @color)
       end
 
+      # Returns the extents for the `Label`'s text.
       def get_extents
-        Vector2.new(Raylib.measure_text_ex(@font, @text, @font_size, @spacing))
+        Dimension.new(Raylib.measure_text_ex(@font, @text, @font_size, @spacing))
       end
 
+      # Draws a `Label`.
       def self.draw(text = DEFAULT_TEXT, position = DEFAULT_POSITION, font_size = DEFAULT_FONT_SIZE, color = DEFAULT_COLOR)
         label = Label.new(text, position, font_size, color)
         label.draw
