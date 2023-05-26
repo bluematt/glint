94f3c6131e10aa16a4a6ab0275aea71c5e67953e
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-26 15:20:06 +0100

    Update example.cr

--- examples/simple_shapes/src/example.cr
+++ examples/simple_shapes/src/example.cr
@@ -12,22 +12,12 @@ class SimpleDrawingExample < Game
 
   # Draw some shapes.
   def draw
-    Line.draw(vec(200, 200), vec(300, 200), 4, color({0, 0, 255}))
-    Line.draw(vec(35, 50), vec(300, 200), 4, color({0, 0, 255, 128}))
-    # Line.draw(vec(100, 100), vec(300, 200), 4, color("ff0000"))
-    # Line.draw(vec(300, 100), vec(100, 200), 4, color(0xf0ff00ffcc))
-    # Circle.draw(vec(300, 300), 50, Color::BLUE, 1, Color::DARKBLUE)
-    # Circle.draw(vec(200, 300), 50, color("#ff0000ff"))
-    # Circle.draw(vec(300, 300), 50, color("#ff0000"))
-    # Circle.draw(vec(400, 300), 50, color("#ff000080"))
-    # Circle.draw(vec(200, 200), 50, color("#ffccccff"))
-    # Circle.draw(vec(300, 200), 50, color("#ffcccc"))
-    # Circle.draw(vec(400, 200), 50, color("#ffcccc80"))
-    # Circle.draw(vec(250, 250), 20, color("f00"), 1, Color::WHITE, OutlinePosition::Inside)
-    # Circle.draw(vec(300, 300), 20, Color::BLUE, 1, Color::DARKBLUE, OutlinePosition::Outside)
-    # Circle.disc(vec(50, 50), 25)
-    # Circle.outline(vec(150, 150), 25)
-    # Circle.draw(vec(25, 250), 10, outline: 5, outline_position: OutlinePosition::Outside)
+    # Red line frmo top left to the center of the window
+    Line.draw(vec(0, 0), vec(400, 300), 4, color({255, 0, 0}))
+    # Blue circle with thick yellow outline at center of window
+    Circle.draw(vec(400, 300), 50, Color::BLUE, 3, Color::YELLOW)
+    # Green square with a thin white outline to right of the circle
+    Square.draw(vec(500, 300), vec(100, 100), Color::GREEN, 1, Color::WHITE)
   end
 end
 
