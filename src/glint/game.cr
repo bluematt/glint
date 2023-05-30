2c5e4d749251c18ece6b1d6627bb06d5f29dfee6
bluematt <307723+bluematt@users.noreply.github.com>
2023-05-30 09:49:21 +0100

    Added accessor methods; refactored defaults and properties

--- src/glint/game.cr
+++ src/glint/game.cr
@@ -4,55 +4,58 @@ module Glint
   # Extend the `Game` class with your own `draw` and `update(delta : Float)` methods
   # to create simple games, or use the `Scene` class to create more complex games.
   class Game
-    DEFAULT_WIDTH     = 1920
-    DEFAULT_HEIGHT    = 1080
-    DEFAULT_TITLE     = "<untitled>"
-    DEFAULT_FRAMERATE = 60
-
-    private START_WAIT_TIME = 0.0000001 # seconds
-    private MINIMUM_DELTA_TIME = 0.0 # seconds
-
     # The game `Window`.
     @window : Window
 
     # The current `Scene`.
     @scene : Scene
 
-    # Returns the game's absolute position.
-    getter position : Vector2 = Vector2.zero
-
-    # The target (maximum ideal) framerate.
-    property target_framerate : Int32 = DEFAULT_FRAMERATE
+    # The game's absolute position.
+    @position : Vector2 = Vector2.zero
 
     # Whether to display FPS.
-    property display_fps : Bool = false
+    @display_fps : Bool = false
+
+    # The position of the FPS label.
+    @@fps_position = Vector2.new(8, 8)
+
+    # The target (maximum ideal) framerate.
+    @@target_framerate : Int32 = 60
 
     # Whether the game is paused.
-    @paused = false
+    @@paused = false
 
-    property fps_position = Vector2.new(8, 8)
+    {% if flag?(:release) %}
+      @log_level = LogLevel::Fatal
+    {% else %}
+      @log_level = LogLevel::All
+    {% end %}
 
     # Creates a new game with optional size, title and framerate.
-    def initialize(width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT, title = DEFAULT_TITLE, @target_framerate = DEFAULT_FRAMERATE)
+    #
+    def initialize(width = 1920.pixels, height = 1080.pixels, title = "<untitled>", @@target_framerate = 60)
+      Raylib.set_trace_log_level(@log_level)
+
       @window = Window.new(width, height, title)
       @scene = Scene.new(Color::BLACK)
     end
 
     # Runs the game's mainloop: handle inputs, update game state, output.
     def run
-      Raylib.set_target_fps(@target_framerate)
+      Raylib.set_target_fps(@@target_framerate)
 
-      Raylib.wait_time(START_WAIT_TIME)
+      # Perform a small wait.
+      Raylib.wait_time(0.0000001)
       delta = Game.frame_time
 
       until should_quit?
-        _update(delta) if delta > MINIMUM_DELTA_TIME
+        _update(delta)
 
         Raylib.begin_drawing
         _draw
         Raylib.end_drawing
 
-        Raylib.set_target_fps(@target_framerate)
+        Raylib.set_target_fps(@@target_framerate)
 
         delta = Game.frame_time
       end
@@ -78,7 +81,7 @@ module Glint
         scene._draw
       end
       draw
-      display_fps_text if @display_fps
+      Game.display_fps_text if @display_fps
     end
 
     # Performs custom drawing.
@@ -113,8 +116,8 @@ module Glint
     end
 
     # Display the current FPS.
-    def display_fps_text
-      Raylib.draw_fps(@fps_position.x, @fps_position.y)
+    def self.display_fps_text
+      Raylib.draw_fps(@@fps_position.x, @@fps_position.y)
     end
 
     # Returns whether the game should quit.
@@ -130,13 +133,99 @@ module Glint
     # Returns the current (real) framerate.
     #
     # This may differ from the `target_framerate`.
-    def framerate
+    def self.framerate
       Raylib.get_fps
+    end
+
+    # :ditto:
+    def framerate
+      Game.framerate
     end
 
     # Returns the duration (in seconds) since the last frame.
     def self.frame_time
       Raylib.get_frame_time
+    end
+
+    # :ditto:
+    def frame_time
+      Game.frame_time
+    end
+
+    # Returns the FPS label's position.
+    def self.fps_position
+      @@fps_position
+    end
+
+    # :ditto:
+    def fps_position
+      Game.fps_position
+    end
+
+    # Sets the FPS label's position.
+    def self.fps_position=(position : Position)
+      @@fps_position = position
+    end
+
+    # :ditto:
+    def fps_position=(position : Position)
+      Game.fps_position = position
+    end
+
+    # Sets the trace log level.
+    def log_level=(log_level : LogLevel)
+      @log_level = log_level
+      Raylib.set_trace_log_level(@log_level)
+    end
+
+    # Returns the trace log level.
+    def log_level
+      @log_level
+    end
+
+    # Set the game's target framerate.
+    def self.target_framerate=(framerate)
+      @@target_framerate = framerate
+    end
+
+    # Return the target framerate.
+    def self.target_framerate
+      @@target_framerate
+    end
+
+    # Set the game's target framerate.
+    def target_framerate=(framerate)
+      Game.target_framerate = framerate
+    end
+
+    # Return the target framerate.
+    def target_framerate
+      Game.target_framerate
+    end
+
+    # Sets whether the game is paused.
+    def self.paused=(paused)
+      @@paused = paused
+    end
+
+    # Returns whether the game is paused.
+    def self.paused?
+      @@paused
+    end
+
+    # Sets whether the game is paused.
+    def paused=(paused)
+      Game.paused = paused
+    end
+
+    # Returns whether the game is paused.
+    def paused?
+      Game.paused?
+    end
+
+    # Returns the game's absolute position.
+    def position
+      @position
     end
   end
 end
